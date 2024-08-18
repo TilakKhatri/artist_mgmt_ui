6f556dd4d463feba8d00e9f2ae53ba3613a6f422
TilakKhatri <tilakkhatri0001@gmail.com>
2024-08-19 00:01:07 +0545

    feat(ui):implement import and export

--- components/tables/artist-table/table.tsx
+++ components/tables/artist-table/table.tsx
@@ -4,13 +4,17 @@ import { useState } from "react";
 import { Plus, ImportIcon, FileSpreadsheet } from "lucide-react";
 import { ColumnDef } from "@tanstack/react-table";
 
-import { Button } from "../../ui/button";
+import { Button, buttonVariants } from "../../ui/button";
 import { Separator } from "../../ui/separator";
 import { DataTable } from "../../ui/data-table";
 
 import { CellAction } from "./cell-action";
 import { IArtist } from "@/types/artist";
 import ArtistModel from "@/components/model/artist-modal";
+import useCsvOperation from "@/hooks/useCsv";
+
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 
 interface IProps {
   currentPage: number;
@@ -27,6 +31,9 @@ export const ArtistClient = ({ data }: { data: IProps }) => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
   const toggle = () => setIsModalOpen((prev) => !prev);
+  const queryClient = useQueryClient();
+
+  const { importCsv } = new useCsvOperation();
 
   const columns: ColumnDef<IArtist>[] = [
     {
@@ -73,6 +80,38 @@ export const ArtistClient = ({ data }: { data: IProps }) => {
     },
   ];
 
+  async function handleExportCsv() {
+    new useCsvOperation().exportCsv();
+  }
+
+  const addArtistMutation = useMutation({
+    mutationFn: async (input: any) => {
+      return importCsv(input);
+    },
+    onSuccess: () => {
+      toast.success("Artist added successfully");
+      queryClient.invalidateQueries({ queryKey: ["artists"] });
+    },
+    onError: (error: any) => {
+      toast.error(error?.response?.data?.message || "Failed to add artist");
+    },
+  });
+
+  async function handleImportCsv(event: any) {
+    event.preventDefault();
+    // console.log("import function call");
+    const file = event.target.files[0];
+
+    if (!file) {
+      toast.error("Please upload file");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("file", file);
+    addArtistMutation.mutate(formData);
+  }
+
   return (
     <>
       <div className="flex items-start justify-end mt-12 space-x-2">
@@ -83,17 +122,22 @@ export const ArtistClient = ({ data }: { data: IProps }) => {
         >
           <Plus className="mr-2 h-4 w-4" /> Add New
         </Button>
-        <Button
-          variant="secondary"
-          className="text-xs md:text-sm"
-          // onClick={() => toggle()}
-        >
-          <ImportIcon className="mr-2 h-4 w-4" /> Import
-        </Button>
+
+        <label className="flex items-center cursor-pointer bg-gray-100 hover:bg-gray-200 text-black rounded-lg py-2 px-4">
+          <ImportIcon className="mr-2 h-4 w-4" />
+          <input
+            type="file"
+            accept=".csv"
+            className="hidden"
+            onChange={handleImportCsv}
+          />
+          Import
+        </label>
+
         <Button
           variant="primary"
           className="text-xs md:text-sm"
-          // onClick={() => toggle()}
+          onClick={handleExportCsv}
         >
           <FileSpreadsheet className="mr-2 h-4 w-4" /> Export
         </Button>
---
+++ hooks/useCsv.tsx
@@ -0,0 +1,45 @@
+import http from "@/lib/http";
+import toast from "react-hot-toast";
+
+class useCsvOperation {
+  exportCsv = async () => {
+    try {
+      const response = await http.get("/artists/export");
+      const blob = new Blob([response.data], { type: "text/csv" });
+
+      // Create a URL for the Blob
+      const url = window.URL.createObjectURL(blob);
+
+      // Create a link element and set the download attributes
+      const link = document.createElement("a");
+      link.href = url;
+      link.setAttribute("download", "artists.csv"); // Specify the file name
+
+      // Append the link to the body and trigger a click
+      document.body.appendChild(link);
+      link.click();
+      console.log(link);
+    } catch (error: any) {
+      console.log(error?.response?.data.message);
+      toast.error(error?.response?.data.message);
+    }
+  };
+
+  importCsv = async (formdata: FormData) => {
+    try {
+      const response = await http.post("/artists/import", formdata, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      console.log("Response: ", response);
+      return response.data;
+    } catch (error: any) {
+      //   console.log("I got error during file upload");
+      //   console.log(error?.message);
+      toast.error(error?.message);
+    }
+  };
+}
+
+export default useCsvOperation;
